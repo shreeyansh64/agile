f7f73115d658fbd8b00ab64860f6e10021be9a77
shreeyansh64 <shreeyansh64@gmail.com>
2025-12-12 18:36:18 +0530

    SignupPage: added gesture detection

--- lib/view/signupEmailPage.dart
+++ lib/view/signupEmailPage.dart
@@ -24,102 +24,105 @@ class _SignupEmailPageState extends State<SignupEmailPage> {
       return (width / 360) * num;
     }
 
-    return Scaffold(
-      body: SizedBox(
-        height: MediaQuery.maybeHeightOf(context),
-        child: Stack(
-          children: [
-            Positioned(
-              top: _responsive(639),
-              left: _responsive(324),
-              child: SvgPicture.asset(
-                'assets/orangeLog.svg',
-                height: _responsive(73),
-                width: _responsive(73),
+    return GestureDetector(
+      onTap: (){FocusScope.of(context).unfocus();},
+      child: Scaffold(
+        body: SizedBox(
+          height: MediaQuery.maybeHeightOf(context),
+          child: Stack(
+            children: [
+              Positioned(
+                top: _responsive(639),
+                left: _responsive(324),
+                child: SvgPicture.asset(
+                  'assets/orangeLog.svg',
+                  height: _responsive(73),
+                  width: _responsive(73),
+                ),
               ),
-            ),
-            Positioned(
-              top: _responsive(109),
-              right: _responsive(323),
-              child: SvgPicture.asset(
-                'assets/tealLog.svg',
-                height: _responsive(73),
-                width: _responsive(73),
+              Positioned(
+                top: _responsive(109),
+                right: _responsive(323),
+                child: SvgPicture.asset(
+                  'assets/tealLog.svg',
+                  height: _responsive(73),
+                  width: _responsive(73),
+                ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsetsGeometry.all(_responsive(20)),
-              child: Column(
-                children: [
-                  Spacer(),
-                  Text(
-                    "Sign Up",
-                    style: AppText.heading1(
-                      context,
-                    ).copyWith(fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: _responsive(40)),
-                  inputField(text: "Email"),
-                  SizedBox(height: _responsive(10)),
-                  inputField(text: "Username"),
-                  SizedBox(height: _responsive(10)),
-                  PasswordField(text: "Password"),
-                  SizedBox(height: _responsive(10)),
-                  PasswordField(text: "Confirm Password"),
-                  SizedBox(height: _responsive(10)),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      CheckboxTheme(
-                        data: CheckboxThemeData(
-                          splashRadius: 20,
-                          fillColor: isChecked
-                              ? MaterialStateProperty.all(
-                                  Appcolors.blue1_normal,
-                                )
-                              : MaterialStateProperty.all(Colors.white),
-                          checkColor: MaterialStateProperty.all(Colors.white),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadiusGeometry.circular(4),
+              Padding(
+                padding: EdgeInsetsGeometry.all(_responsive(20)),
+                child: Column(
+                  children: [
+                    Spacer(),
+                    Text(
+                      "Sign Up",
+                      style: AppText.heading1(
+                        context,
+                      ).copyWith(fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: _responsive(40)),
+                    inputField(text: "Email"),
+                    SizedBox(height: _responsive(10)),
+                    inputField(text: "Username"),
+                    SizedBox(height: _responsive(10)),
+                    PasswordField(text: "Password"),
+                    SizedBox(height: _responsive(10)),
+                    PasswordField(text: "Confirm Password"),
+                    SizedBox(height: _responsive(10)),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        CheckboxTheme(
+                          data: CheckboxThemeData(
+                            splashRadius: 20,
+                            fillColor: isChecked
+                                ? MaterialStateProperty.all(
+                                    Appcolors.blue1_normal,
+                                  )
+                                : MaterialStateProperty.all(Colors.white),
+                            checkColor: MaterialStateProperty.all(Colors.white),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadiusGeometry.circular(4),
+                            ),
+                          ),
+                          child: Transform.scale(
+                            scale: _responsive(0.8),
+                            child: Checkbox(
+                              value: isChecked,
+                              onChanged: (v) {
+                                setState(() {
+                                  isChecked = v!;
+                                });
+                              },
+                            ),
                           ),
                         ),
-                        child: Transform.scale(
-                          scale: _responsive(0.8),
-                          child: Checkbox(
-                            value: isChecked,
-                            onChanged: (v) {
-                              setState(() {
-                                isChecked = v!;
-                              });
-                            },
+                        Text(
+                          "I Agree with the ",
+                          style: AppText.textButton(context),
+                        ),
+                        Text(
+                          "Terms and Policy",
+                          style: AppText.textButton(context).copyWith(
+                            color: Appcolors.blue1_normal,
+                            decoration: TextDecoration.underline,
+                            decorationColor: Appcolors.blue1_normal,
                           ),
                         ),
-                      ),
-                      Text(
-                        "I Agree with the ",
-                        style: AppText.textButton(context),
-                      ),
-                      Text(
-                        "Terms and Policy",
-                        style: AppText.textButton(context).copyWith(
-                          color: Appcolors.blue1_normal,
-                          decoration: TextDecoration.underline,
-                          decorationColor: Appcolors.blue1_normal,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: _responsive(40)),
-                  BlueButton(text: "Sign Up", function: () {}),
-                  Spacer(),
-                ],
+                      ],
+                    ),
+                    SizedBox(height: _responsive(40)),
+                    BlueButton(text: "Sign Up", function: () {}),
+                    Spacer(),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.startTop,
+        floatingActionButton: floatBackButton(),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.startTop,
-      floatingActionButton: floatBackButton(),
     );
   }
 }
