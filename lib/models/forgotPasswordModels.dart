b95dd3e41557e201dc51ca06056ec580a4c4f55c
shreeyansh64 <shreeyansh64@gmail.com>
2025-12-14 04:15:23 +0530

    ForgotPassword: added request model

--- lib/models/forgotPasswordModels.dart
+++ lib/models/forgotPasswordModels.dart
@@ -11,3 +11,14 @@ class ForgotPasswordResponse {
     );
   }
 }
+
+class ForgotPasswordRequest {
+  final String email;
+
+  ForgotPasswordRequest({required this.email});
+
+  Map<String, dynamic> toJson() => {
+        "email": email,
+      };
+}
+
